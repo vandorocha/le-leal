00d475341b030dea103a3e4ee1f319b4e287c656
vandorocha <vandorcdev@gmail.com>
2024-09-27 13:44:16 -0300

    first commit

---
+++ src/js/engine.js
@@ -0,0 +1,82 @@
+const state = {
+    view: {
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
+    },
+
+    values:{
+        gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
+        currentTime: 60,
+    },
+
+    actions:{
+        timeId: setInterval(randomSquare, 1000),
+        countDownTimerId: setInterval(countDown, 1000), 
+    }
+
+};
+
+
+function playSound(audioName){
+    let audio = new Audio(`./src/audio/${audioName}.m4a`)
+    audio.volume = 0.2;
+    audio.play();
+
+}
+
+
+function countDown(){
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime;
+    if(state.values.currentTime <= 0){
+        clearInterval(state.actions.countDownTimerId);
+        clearInterval(state.actions.timeId);
+
+        alert("Game Over! O seu resultado foi: " + state.values.result);
+    }
+}
+
+function randomSquare(){
+    state.view.squares.forEach((square) => {
+        square.classList.remove("enemy");
+    })
+
+    let randomNumber = Math.floor(Math.random()*9);
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id
+}
+
+function addListenerHitBox(){
+    state.view.squares.forEach((square) => {
+        square.addEventListener("mousedown", ()=> {
+            if(square.id === state.values.hitPosition){
+                state.values.result++;
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+                playSound("hit");
+            }
+        })
+    })
+};
+
+function init(){
+    addListenerHitBox()
+};
+init();
+
+// Cria o elemento de áudio
+const audio = new Audio('dingo.m4a');
+  
+// Define as propriedades do áudio
+audio.autoplay = true; // Para iniciar automaticamente
+audio.loop = true; // Para repetir continuamente
+
+// Adiciona um evento para iniciar o áudio após o carregamento da página
+window.addEventListener('load', function() {
+audio.play(); // Inicia o áudio
+});
